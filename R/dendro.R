1ffe516effb944151cf36e1117f2d5ea7a094ae2
Liudas-Dau <liudasdau@gmail.com>
2024-12-01 23:27:21 +0200

    Fix node positions bug and add visualization parameters (type, label_size)

--- R/dendro.R
+++ R/dendro.R
@@ -2,12 +2,14 @@
 #'
 #' @description This function displays a dendrogram of polygons produced by hespdiv split-lines. Branch length is proportional to difference. If performance of split-lines is a similarity measure, it is internally converted to difference.
 #' @param obj A hespdiv object.
+#' @param type integer 1 (floating endnodes) or 2 (endnotes at zero distance).
 #' @param poly.scheme ggplot2 object produced with poly_scheme function. Provide if you want identical colors for polygons in both plots.
 #' @param color color vector used for dendrogram nodes and branches.
 #' @param performance.col color vector used for text, displaying difference values between polygons
 #' @param labels.col color vector used for text, displaying polygon IDs.
 #' @param offset.factor numeric value used to scale the offset distance of displayed polygon IDs and performance values from a dendrogram node. Adjust experimentally, if you don't like the current distance.
 #' @param arrange logical. Plot hespdiv dendrogram above polygon scheme?
+#' @param label.size size of labels.
 #' @param grob logical. Convert plot to grob? Must be true, if you want to arrange polygon scheme and the dendrogram in a single plot.
 #' @return A `grob` or `TableGrob` object if `grob = TRUE`, otherwise `NULL`.
 #' @importFrom igraph graph_from_data_frame V E ends vcount layout_as_tree
@@ -27,11 +29,13 @@
 #' # dendro(HDData::hd, scheme, arrange = FALSE)
 #' ## When arranging plots, you may need to experimentally adjust offset.factor.
 #' # dendro(HDData::hd, scheme, offset.factor = 2)
+#' ## Classical dendrogram
+#' # dendro(HDData::hd, type = 2)
 #' ## End(Not run)
 #' @export
-dendro <- function(obj, poly.scheme = NULL, color = 1, performance.col = "blue",
+dendro <- function(obj, type = 1, poly.scheme = NULL, color = 1, performance.col = "blue",
                    labels.col = 1, offset.factor = 1, arrange = TRUE,
-                   grob = TRUE){
+                   grob = TRUE, label.size = 0.5){
   if (arrange & !grob) {
     stop("Can only arrange polygon scheme and dendrogram in a single plot,
          when dendrogram is grob.")
@@ -41,9 +45,9 @@ dendro <- function(obj, poly.scheme = NULL, color = 1, performance.col = "blue",
 
     # Wrapper function to call .base_dendro
     .call_base <- function() {
-      .base_dendro(obj, poly.scheme = poly.scheme, color = color,
+      .base_dendro(obj, type = type, poly.scheme = poly.scheme, color = color,
                    performance.col = performance.col, labels.col = labels.col,
-                   offset.factor = offset.factor)
+                   offset.factor = offset.factor, label.size = label.size)
     }
 
     grob <- grid::grid.grabExpr(gridGraphics::grid.echo(
@@ -52,23 +56,26 @@ dendro <- function(obj, poly.scheme = NULL, color = 1, performance.col = "blue",
     if (arrange & !is.null(poly.scheme)){
       gridExtra::grid.arrange(grob, poly.scheme, ncol = 1)
     } else {
-      grid::grid.newpage()
+
       grid::grid.draw(grob)
       grob
     }
   } else {
-    .base_dendro(obj, poly.scheme = poly.scheme, color = color,
+    .base_dendro(obj, type = type, poly.scheme = poly.scheme, color = color,
                  performance.col = performance.col, labels.col = labels.col,
-                 offset.factor = offset.factor)
+                 offset.factor = offset.factor, label.size)
   }
 
 }
 
 # generate the dendrogram using base R graphics
 #' @noRd
-.base_dendro <- function(obj, poly.scheme = NULL, color = 1,
-                         performance.col = "blue",
-                         labels.col = 1, offset.factor = 1 ){
+.base_dendro <- function(obj, type = type, poly.scheme = poly.scheme,
+                         color = color,
+                         performance.col = performance.col,
+                         labels.col = labels.col,
+                         offset.factor = offset.factor,
+                         label.size){
   # extract essential data
   pols <- obj$poly.stats[, c("plot.id", "root.id", "has.split")]
   pols$performance <- ifelse(
@@ -132,18 +139,54 @@ dendro <- function(obj, poly.scheme = NULL, color = 1, performance.col = "blue",
     vertex_map[as.character(igraph::ends(g,igraph::E(g))[, 2])]]
 
   # Step 4: Calculate node positions manually
-  node_positions <- numeric(length(vertex_map))  # Initialize positions for all nodes
-  for (edge in igraph::E(g)) {
-    parent <- vertex_map[as.character(igraph::ends(g, edge)[1])]  # Map parent to index
-    child <- vertex_map[as.character(igraph::ends(g, edge)[2])]  # Map child to index
-    node_positions[child] <- node_positions[parent] + igraph::E(g)$weight[edge]
+  end_nodes_id <- obj$poly.stats$plot.id[!obj$poly.stats$has.split]
+  x_kords <- rep(NaN, nrow(obj$poly.stats))
+  names(x_kords) <- obj$poly.stats$plot.id
+  x_kords[as.character(end_nodes_id)] <- 1:length(end_nodes_id)
+  y_koords <- rep(NaN, nrow(obj$poly.stats))
+  names(y_koords) <- obj$poly.stats$plot.id
+  end_node_parents <- sapply(end_nodes_id, .find_pol_parents, obj = obj)
+
+  if (type == 1) {
+    y_koords["1"] <- 0
+    node_parents <- sapply(obj$poly.stats$plot.id[-1], .find_pol_parents, obj = obj)
+    y_koords[as.character(obj$poly.stats$plot.id[-1])] <-
+      unlist(lapply(node_parents, function(ids,obj) {
+        sum(pols$branch_length[which(pols$plot.id %in% ids)])
+      },
+      obj = obj))
+
+
+  } else {
+    y_koords[as.character(end_nodes_id)] <- 0
   }
 
-  # Step 5: Create a layout reflecting the computed node positions
-  layout <- igraph::layout_as_tree(g)  # Use tree layout for x-coordinates
-  layout[, 2] <- -node_positions  # Set y-coordinates to the computed positions (invert to go downward)
+  siblings <- which(duplicated(end_node_parents))
+  while(length(siblings) > 0){
+    parent_id <- sapply(end_node_parents[siblings], `[`, 1)
+    oldparents <- lapply(end_node_parents[siblings], `[`, -1)
+    x_kords[as.character(parent_id)] <-
+      (x_kords[as.character(end_nodes_id[siblings])] +
+         x_kords[as.character(end_nodes_id[siblings-1])]) / 2
+    if (type != 1) {
+      y_koords[as.character(parent_id)] <-
+        pols$branch_length[pols$plot.id %in% parent_id] +
+        apply(data.frame(y_koords[as.character(end_nodes_id[siblings])],
+                         y_koords[as.character(end_nodes_id[siblings-1])]),1,max)
+    }
+    end_nodes_id[c(siblings-1)] <- parent_id
+    end_nodes_id <- end_nodes_id[-siblings]
+    end_node_parents[siblings -1] <- oldparents
+    end_node_parents <- end_node_parents[-siblings]
+    siblings <- which(duplicated(end_node_parents))
+  }
+  if (type == 1 ) {
+    layout <- as.matrix(data.frame(x_kords,-y_koords))
+  } else {
+    layout <- as.matrix(data.frame(x_kords,y_koords))
+  }
 
-  # Step 6: Prepare data for lateral-first edges
+  # Step 5: Prepare data for lateral-first edges
   edge_segments <- data.frame(
     x0 = layout[vertex_map[as.character(igraph::ends(g, igraph::E(g))[, 1])], 1],  # Parent x
     y0 = layout[vertex_map[as.character(igraph::ends(g, igraph::E(g))[, 1])], 2],  # Parent y
@@ -151,7 +194,7 @@ dendro <- function(obj, poly.scheme = NULL, color = 1, performance.col = "blue",
     y1 = layout[vertex_map[as.character(igraph::ends(g, igraph::E(g))[, 2])], 2],  # Child y
     weight = igraph::E(g)$weight  # Edge weight for segment length
   )
-  # Step 7: Plot the dendrogram with lateral-first edges
+  # Step 6: Plot the dendrogram with lateral-first edges
   plot(
     NULL,
     xlim = range(layout[, 1])*1.1,  # Set x-axis limits
@@ -190,7 +233,7 @@ dendro <- function(obj, poly.scheme = NULL, color = 1, performance.col = "blue",
           length(unique(obj$split.stats$rank)),  # Slightly below the parent node
         labels = round(edge_segments$weight[i], 2),
         col = performance.col,
-        cex = 0.8
+        cex = label.size
       )
     }
   }
@@ -212,16 +255,31 @@ dendro <- function(obj, poly.scheme = NULL, color = 1, performance.col = "blue",
     labels = igraph::V(g)$plot_id,
     col = labels.col,
     pos = NULL,  # Place them directly below
-    cex = 0.8
+    cex = label.size
   )
 
   rounded_y_ticks <- round(seq(min(layout[, 2]), max(layout[, 2]), length.out = 5), 2)
-
+  if (type == 1) {
+  labels <- -rounded_y_ticks
+  } else {
+    labels <- rounded_y_ticks
+  }
   graphics::axis(
     side = 2,                                # Left-side Y-axis
     at = rounded_y_ticks,                    # Rounded tick positions
     labels = -rounded_y_ticks,                # Rounded tick labels
     las = 1,                                 # Rotate labels to be horizontal
-    line = -0.5                              # Adjust spacing of axis to prevent overlap
+    line = 1                              # Adjust spacing of axis to prevent overlap
   )
+}
+
+#' @noRd
+.find_pol_parents <- function(obj,id){
+  parent_id <- obj$poly.stats$root.id[id]
+  ids <- parent_id
+  while (parent_id != 1){
+    parent_id <- obj$poly.stats$root.id[parent_id]
+    ids <- c(ids, parent_id)
+  }
+  ids
 }
