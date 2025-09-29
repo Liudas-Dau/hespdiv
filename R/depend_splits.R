f20bf64c28c07364ae993ecbb3e4ec60ac006289
Liudas-Dau <liudasdau@gmail.comm>
2025-09-29 16:47:21 +0300

    add comments and review. Fix documentation formatting

--- R/depend_splits.R
+++ R/depend_splits.R
@@ -3,19 +3,26 @@
 #' @description  For a provided split-line id, function returns a list of
 #' all offspring polygons and split-lines, if there are any.
 #'
-#' @return a list
-#' \itemize{
-#' \item{1)}{ dependent split-line ids, if any}
-#' \item{2)}{ dependent polygon ids}
+#' @return a list:
+#' \enumerate{
+#'   \item Dependent split-line IDs, if any
+#'   \item Dependent polygon IDs
 #' }
 #' @param obj hespdiv object
 #' @param id id of a split-line
 #' @export
 depend_splits <- function(obj, id){
-  offspring <- .dep_split(obj, id)
+  offspring <- .dep_split(obj, id) # obtain dependent split ids and polygon ids
+
+  # add offspring to a total vector of offspring:
   all_depend_splits <- offspring$splits
   all_depend_pols <- offspring$polygons
+
+  # create a vector of split-line still needed to check for offspring
   unchecked <- offspring$splits
+
+  # while there are unchecked offspring split-lines, obtain offspring of each
+  # offspring
   while (length(unchecked) > 0){
     new <- numeric()
     for (split.id in unchecked){
@@ -25,7 +32,7 @@ depend_splits <- function(obj, id){
       all_depend_pols <- c(all_depend_pols, offspring$polygons)
 
     }
-    unchecked <- new
+    unchecked <- new # reset to new list of offspring split-lines
   }
   return(list(
     splits = all_depend_splits,
@@ -39,9 +46,9 @@ depend_splits <- function(obj, id){
   # Step 1: identify produced polygons
   offspring_id <-
     which(obj$poly.stats$root.id == obj$split.stats$plot.id[id])
-  # Step 2: dependent split(s)
+  # Step 2: does each polygon has a split-line?
   depend_l <- obj$poly.stats[offspring_id, "has.split"]
-  # if any return ids
+  # if any TRUE, return split ids + polygon ids; else return only polygon ids
   if (any(depend_l)) {
     list(splits = which(obj$split.stats$plot.id %in% offspring_id[depend_l]),
          polygons = offspring_id)
