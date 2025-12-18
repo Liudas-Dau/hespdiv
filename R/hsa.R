c966dc9fc26bc43471413512387ddd81b6f63a78
Liudas-Dau <liudasdau@gmail.comm>
2025-12-18 17:10:56 +0200

    validate input data structures, fix naming, fix docs, fix use.chull sampling.

--- R/hsa.R
+++ R/hsa.R
@@ -43,7 +43,8 @@
 #'   (default), images are not saved.
 #'
 #' @param pnts.col Value passed to the \code{pnts.col} argument of each
-#'   \code{hespdiv} call.
+#'   \code{hespdiv} call. Can be provided as atomic vector,  vector or list of
+#'   vectors.
 #'
 #' @param data A list of data objects (matrices, data frames, vectors, lists,
 #'   or other supported data structures) used as alternative inputs for
@@ -164,6 +165,35 @@ hsa <- function(obj,
                 future_seed = TRUE) {
 
   if (!inherits(obj, "hespdiv")) stop("obj should have 'hespdiv' class.")
+
+  .check_hsa_arg_structures(
+    data = data,
+    xy.dat = xy.dat,
+    study.pol = study.pol,
+    same.n.split = same.n.split,
+    c.fast.optim = c.fast.optim,
+    use.chull = use.chull,
+    c.splits = c.splits,
+    n.split.pts = n.split.pts,
+    c.max.iter.no = c.max.iter.no,
+    N.crit = N.crit,
+    N.loc.crit = N.loc.crit,
+    c.X.knots = c.X.knots,
+    c.Y.knots = c.Y.knots,
+    N.rel.crit = N.rel.crit,
+    N.loc.rel.crit = N.loc.rel.crit,
+    S.crit = S.crit,
+    S.rel.crit = S.rel.crit,
+    Q.crit = Q.crit,
+    c.Q.crit = c.Q.crit,
+    c.crit.improv = c.crit.improv,
+    c.corr.term = c.corr.term,
+    generalize.f = generalize.f,
+    compare.f = compare.f,
+    maximize = maximize,
+    method = method
+  )
+
   if (is.null(.run.id)) .run.id <- -1
 
   if (!is.numeric(n.runs) || n.runs < 1 || n.runs %% 1 || length(n.runs) != 1)
@@ -184,7 +214,7 @@ hsa <- function(obj,
     if (!is.null(xy.dat)) xy.len <- length(xy.dat)
   }
 
-  # Ensure basal data/xy included
+  # basal data
   if ((!list(obj$call.info$Call_ARGS$xy.dat) %in% xy.dat |
        !list(obj$call.info$Call_ARGS$data) %in% data)) {
     if (data.paired) {
@@ -199,7 +229,7 @@ hsa <- function(obj,
   same.n.split <- .set("same.n.split", obj)
   n.split.pts  <- .set("n.split.pts", obj)
 
-  # Custom method bookkeeping (kept as in your code)
+  # Custom method bookkeeping
   is.custom <- (!is.null(maximize) | obj$call.info$METHOD$method.type == "custom")
 
   if (obj$call.info$METHOD$method.type == "custom" &
@@ -278,128 +308,6 @@ hsa <- function(obj,
   hes.res <- vector("list", length = n.runs)
   names(hes.res) <- as.character(run_ids)
 
-  # ---- helper: draw arguments for run i (same logic as your loop) ----
-  draw_one <- function(i) {
-    p.id <- sample.int(dat_len, 1)
-
-    local_use_m <- use_m
-    if (m.compete) local_use_m <- sample(0:1, 1, prob = c(length(maximize), length(method)))
-
-    if (!local_use_m) cm.id <- sample.int(len_cm, 1) else cm.id <- NA_integer_
-
-    v1 <- data[[p.id]]
-    v2 <- if (data.paired) xy.dat[[p.id]] else sample(xy.dat, 1)[[1]]
-    v3 <- same.n.split[sample.int(length(same.n.split), 1)]
-    v4 <- n.split.pts[sample.int(length(n.split.pts), 1)]
-    v5 <- .ifelse(!local_use_m, generalize.f[[cm.id]], NULL)
-    v6 <- .ifelse(!local_use_m, maximize[cm.id], NULL)
-    v7 <- .ifelse(!local_use_m, compare.f[[cm.id]], NULL)
-    v8 <- .ifelse(local_use_m, sample(method, 1), NULL)
-    v9 <- N.crit[sample.int(length(N.crit), 1)]
-    v10 <- N.rel.crit[sample.int(length(N.rel.crit), 1)]
-    v11 <- N.loc.crit[sample.int(length(N.loc.crit), 1)]
-    v12 <- N.loc.rel.crit[sample.int(length(N.loc.rel.crit), 1)]
-    v13 <- S.crit[sample.int(length(S.crit), 1)]
-    v14 <- S.rel.crit[sample.int(length(S.rel.crit), 1)]
-    v15 <- .ifelse(local_use_m,
-                   ifelse(v8 == "pielou",
-                          (Q.crit[Q.crit < 1])[
-                            sample.int(length(Q.crit[Q.crit < 1]), 1)
-                            ],
-                          (Q.crit[Q.crit > 0])[
-                            sample.int(length(Q.crit[Q.crit > 0]), 1)
-                          ]),
-                   NULL)
-    v16 <- c.splits[sample.int(length(c.splits), 1)]
-    is.maxim <- ifelse(local_use_m, v8 == "pielou", v6)
-
-    if (is.null(v15)) {
-      v17 <- NULL
-    } else {
-      v17 <- .ifelse(v16,
-                     ifelse(is.maxim, (c.Q.crit[c.Q.crit <= v15])[
-                       sample.int(length(c.Q.crit[c.Q.crit <= v15]), 1)
-                     ],
-                     (c.Q.crit[c.Q.crit >= v15])[
-                       sample.int(length(c.Q.crit[c.Q.crit >= v15]), 1)
-                     ]),
-                     NULL)
-    }
-
-    v18 <- .ifelse(v16, c.crit.improv[sample.int(length(c.crit.improv), 1)], NULL)
-    v19 <- .ifelse(v16, c.X.knots[sample.int(length(c.X.knots), 1)], NULL)
-    v20 <- .ifelse(v16, c.Y.knots[sample.int(length(c.Y.knots), 1)], NULL)
-    v21 <- .ifelse(v16, c.max.iter.no[sample.int(length(c.max.iter.no), 1)], NULL)
-    v22 <- .ifelse(v16, c.fast.optim[sample.int(length(c.fast.optim), 1)], NULL)
-    v23 <- .ifelse(v16, c.corr.term[sample.int(length(c.corr.term), 1)], NULL)
-    v24 <- sample(0:1, 1, prob = c(length(study.pol), ifelse(any(use.chull), 1, 0)))
-    v25 <- .ifelse(v24, obj$call$Call_ARGS$study.pol, sample(study.pol, 1))[[1]]
-    v26 <- .ifelse(display, .ifelse(is.pnts, pnts.col[[p.id]], pnts.col)[[1]], NULL)
-
-    list(v1=v1, v2=v2, v3=v3, v4=v4, v5=v5, v6=v6, v7=v7, v8=v8, v9=v9, v10=v10,
-         v11=v11, v12=v12, v13=v13, v14=v14, v15=v15, v16=v16, v17=v17, v18=v18,
-         v19=v19, v20=v20, v21=v21, v22=v22, v23=v23, v24=v24, v25=v25, v26=v26)
-  }
-
-  # ---- helper: evaluate one run given a draw ----
-  eval_one <- function(i, d) {
-
-    if (!is.null(images.path)) {
-      fn <- file.path(images.path, sprintf("hsa_run_%04d.png", i))
-      png(fn)
-      on.exit(dev.off(), add = TRUE)
-    }
-
-    call_args <- quote(
-      hespdiv(
-        data = v1,
-        xy.dat = v2,
-        same.n.split = v3,
-        n.split.pts = v4,
-        generalize.f = v5,
-        maximize = v6,
-        compare.f = v7,
-        method = v8,
-        N.crit = v9,
-        N.rel.crit = v10,
-        N.loc.crit = v11,
-        N.loc.rel.crit = v12,
-        S.crit = v13,
-        S.rel.crit = v14,
-        Q.crit = v15,
-        c.splits = v16,
-        c.Q.crit = v17,
-        c.crit.improv = v18,
-        c.X.knots = v19,
-        c.Y.knots = v20,
-        c.max.iter.no = v21,
-        c.fast.optim = v22,
-        c.corr.term = v23,
-        use.chull = v24,
-        study.pol = v25,
-        tracing = NULL,
-        pnts.col = v26,
-        display = display,
-        pacific.region = pacific.region
-      )
-    )
-
-    env <- list2env(c(d, list(display = display, pacific.region = pacific.region)),
-                    parent = parent.frame())
-
-    out <- tryCatch(
-      eval(call_args, envir = env),
-      warning = function(w) {
-        list(Subdivision = suppressWarnings(eval(call_args, envir = env)),
-             warning = w)
-      },
-      error = function(e) {
-        list(Arguments = as.list(d), error = e)
-      }
-    )
-
-    list(Subdivison = out, Arguments = as.list(d))
-  }
 
   # =========================
   # SERIAL PATH
@@ -537,4 +445,249 @@ check.errs <- function(hes.res, .message = TRUE){
     }
   }
   x
+}
+#' evaluate one run given a draw
+#' @noRd
+eval_one <- function(i, d) {
+
+  if (!is.null(images.path)) {
+    fn <- file.path(images.path, sprintf("hsa_run_%04d.png", i))
+    png(fn)
+    on.exit(dev.off(), add = TRUE)
+  }
+
+  call_args <- quote(
+    hespdiv(
+      data = data,
+      xy.dat = xy.dat,
+      same.n.split = same.n.split,
+      n.split.pts = n.split.pts,
+      generalize.f = generalize.f,
+      maximize = maximize,
+      compare.f = compare.f,
+      method = method,
+      N.crit = N.crit,
+      N.rel.crit = N.rel.crit,
+      N.loc.crit = N.loc.crit,
+      N.loc.rel.crit = N.loc.rel.crit,
+      S.crit = S.crit,
+      S.rel.crit = S.rel.crit,
+      Q.crit = Q.crit,
+      c.splits = c.splits,
+      c.Q.crit = c.Q.crit,
+      c.crit.improv = c.crit.improv,
+      c.X.knots = c.X.knots,
+      c.Y.knots = c.Y.knots,
+      c.max.iter.no = c.max.iter.no,
+      c.fast.optim = c.fast.optim,
+      c.corr.term = c.corr.term,
+      use.chull = use.chull,
+      study.pol = study.pol,
+      tracing = NULL,
+      pnts.col = pnts.col,
+      display = display,
+      pacific.region = pacific.region
+    )
+  )
+
+  env <- list2env(c(d, list(display = display, pacific.region = pacific.region)),
+                  parent = parent.frame())
+
+  out <- tryCatch(
+    eval(call_args, envir = env),
+    warning = function(w) {
+      list(Subdivision = suppressWarnings(eval(call_args, envir = env)),
+           warning = w)
+    },
+    error = function(e) {
+      list(Arguments = as.list(d), error = e)
+    }
+  )
+
+  list(Subdivison = out, Arguments = as.list(d))
+}
+
+#' draw arguments for run i
+#' @noRd
+draw_one <- function(i) {
+  p.id <- sample.int(dat_len, 1)
+
+  local_use_m <- use_m
+  if (m.compete) local_use_m <- sample(0:1, 1, prob = c(length(maximize), length(method)))
+
+  if (!local_use_m) cm.id <- sample.int(len_cm, 1) else cm.id <- NA_integer_
+
+  v1 <- data[[p.id]]
+  v2 <- if (data.paired) xy.dat[[p.id]] else sample(xy.dat, 1)[[1]]
+  v3 <- same.n.split[sample.int(length(same.n.split), 1)]
+  v4 <- n.split.pts[sample.int(length(n.split.pts), 1)]
+  v5 <- .ifelse(!local_use_m, generalize.f[[cm.id]], NULL)
+  v6 <- .ifelse(!local_use_m, maximize[cm.id], NULL)
+  v7 <- .ifelse(!local_use_m, compare.f[[cm.id]], NULL)
+  v8 <- .ifelse(local_use_m, sample(method, 1), NULL)
+  v9 <- N.crit[sample.int(length(N.crit), 1)]
+  v10 <- N.rel.crit[sample.int(length(N.rel.crit), 1)]
+  v11 <- N.loc.crit[sample.int(length(N.loc.crit), 1)]
+  v12 <- N.loc.rel.crit[sample.int(length(N.loc.rel.crit), 1)]
+  v13 <- S.crit[sample.int(length(S.crit), 1)]
+  v14 <- S.rel.crit[sample.int(length(S.rel.crit), 1)]
+  v15 <- .ifelse(local_use_m,
+                 ifelse(v8 == "pielou",
+                        (Q.crit[Q.crit < 1])[
+                          sample.int(length(Q.crit[Q.crit < 1]), 1)
+                        ],
+                        (Q.crit[Q.crit > 0])[
+                          sample.int(length(Q.crit[Q.crit > 0]), 1)
+                        ]),
+                 NULL)
+  v16 <- c.splits[sample.int(length(c.splits), 1)]
+  is.maxim <- ifelse(local_use_m, v8 == "pielou", v6)
+
+  if (is.null(v15)) {
+    v17 <- NULL
+  } else {
+    v17 <- .ifelse(v16,
+                   ifelse(is.maxim, (c.Q.crit[c.Q.crit <= v15])[
+                     sample.int(length(c.Q.crit[c.Q.crit <= v15]), 1)
+                   ],
+                   (c.Q.crit[c.Q.crit >= v15])[
+                     sample.int(length(c.Q.crit[c.Q.crit >= v15]), 1)
+                   ]),
+                   NULL)
+  }
+
+  v18 <- .ifelse(v16, c.crit.improv[sample.int(length(c.crit.improv), 1)], NULL)
+  v19 <- .ifelse(v16, c.X.knots[sample.int(length(c.X.knots), 1)], NULL)
+  v20 <- .ifelse(v16, c.Y.knots[sample.int(length(c.Y.knots), 1)], NULL)
+  v21 <- .ifelse(v16, c.max.iter.no[sample.int(length(c.max.iter.no), 1)], NULL)
+  v22 <- .ifelse(v16, c.fast.optim[sample.int(length(c.fast.optim), 1)], NULL)
+  v23 <- .ifelse(v16, c.corr.term[sample.int(length(c.corr.term), 1)], NULL)
+  v24 <- use.chull[sample.int(length(use.chull), 1)]
+  v25 <- .ifelse(v24, obj$call$Call_ARGS$study.pol, sample(study.pol, 1)[[1]])
+  v26 <- .ifelse(display, .ifelse(is.pnts, pnts.col[[p.id]], pnts.col), NULL)
+
+  list(data=v1, xy.dat=v2, same.n.split=v3, n.split.pts=v4, generalize.f=v5,
+       v6=maximize, maximize=v7, method=v8, N.crit=v9, N.rel.crit=v10,
+       N.loc.crit=v11, N.loc.rel.crit=v12, S.crit=v13, S.rel.crit=v14,
+       Q.crit=v15, c.splits=v16, c.Q.crit=v17, c.crit.improv=v18,
+       c.X.knots=v19, c.Y.knots=v20, c.max.iter.no=v21, c.fast.optim=v22,
+       c.corr.term=v23, use.chull=v24, study.pol=v25, is.pnts=v26)
+}
+
+#' @noRd
+.check_hsa_arg_structures <- function(
+    data = NULL,
+    xy.dat = NULL, study.pol = NULL,
+    same.n.split = NULL, c.fast.optim = NULL, use.chull = NULL, c.splits = NULL,
+    n.split.pts = NULL, c.max.iter.no = NULL, N.crit = NULL, N.loc.crit = NULL,
+    c.X.knots = NULL, c.Y.knots = NULL,
+    N.rel.crit = NULL, N.loc.rel.crit = NULL, S.crit = NULL, S.rel.crit = NULL,
+    Q.crit = NULL, c.Q.crit = NULL, c.crit.improv = NULL,
+    c.corr.term = NULL,
+    generalize.f = NULL, compare.f = NULL,
+    maximize = NULL,
+    method = NULL
+) {
+  .bad <- function(name, msg) stop(paste0("'", name, "': ", msg), call. = FALSE)
+
+  .is_intish <- function(x) is.numeric(x) && all(is.finite(x)) && all(x %% 1 == 0)
+
+  .chk_list <- function(x, name) {
+    if (is.null(x)) return(invisible(TRUE))
+    if (!is.list(x)) .bad(name, "must be a list (or NULL).")
+    invisible(TRUE)
+  }
+
+  .chk_vec_type <- function(x, name, type = c("logical","numeric","integer","character")) {
+    if (is.null(x)) return(invisible(TRUE))
+    type <- match.arg(type)
+    ok <- switch(
+      type,
+      logical   = is.logical(x),
+      numeric   = is.numeric(x),
+      integer   = .is_intish(x),
+      character = is.character(x)
+    )
+    if (!ok) .bad(name, paste0("must be a ", type, " vector (or NULL)."))
+    invisible(TRUE)
+  }
+
+  .chk_range <- function(x, name, lo, hi) {
+    if (is.null(x)) return(invisible(TRUE))
+    if (!is.numeric(x)) .bad(name, "must be a numeric vector (or NULL).")
+    if (any(x < lo | x > hi, na.rm = TRUE)) {
+      .bad(name, paste0("values must be in [", lo, ", ", hi, "]."))
+    }
+    invisible(TRUE)
+  }
+
+  .chk_xy_list <- function(x, name) {
+    if (is.null(x)) return(invisible(TRUE))
+    if (!is.list(x)) .bad(name, "must be a list of data.frames (or NULL).")
+    for (i in seq_along(x)) {
+      d <- x[[i]]
+      if (!is.data.frame(d)) .bad(name, paste0("element ", i, " must be a data.frame."))
+      if (ncol(d) != 2) .bad(name, paste0("element ", i, " must have exactly 2 columns: x and y."))
+      if (!all(c("x","y") %in% names(d))) .bad(name, paste0("element ", i, " must have columns named 'x' and 'y'."))
+      if (!is.numeric(d[["x"]]) || !is.numeric(d[["y"]])) .bad(name, paste0("element ", i, " columns 'x' and 'y' must be numeric."))
+    }
+    invisible(TRUE)
+  }
+
+  .chk_fun_list <- function(x, name) {
+    if (is.null(x)) return(invisible(TRUE))
+    if (!is.list(x)) .bad(name, "must be a list of functions (or NULL).")
+    if (any(!vapply(x, is.function, logical(1)))) .bad(name, "must contain only functions.")
+    invisible(TRUE)
+  }
+
+  # data: list
+  .chk_list(data, "data")
+
+  # xy.dat, study.pol: lists of 2-col data.frames (x,y)
+  .chk_xy_list(xy.dat, "xy.dat")
+  .chk_xy_list(study.pol, "study.pol")
+
+  # logical vectors
+  .chk_vec_type(same.n.split, "same.n.split", "logical")
+  .chk_vec_type(c.fast.optim, "c.fast.optim", "logical")
+  .chk_vec_type(use.chull,    "use.chull",    "logical")
+  .chk_vec_type(c.splits,     "c.splits",     "logical")
+
+  # integer vectors (integer-like numeric)
+  .chk_vec_type(n.split.pts,   "n.split.pts",   "integer")
+  .chk_vec_type(c.max.iter.no, "c.max.iter.no", "integer")
+  .chk_vec_type(N.crit,        "N.crit",        "integer")
+  .chk_vec_type(N.loc.crit,    "N.loc.crit",    "integer")
+  .chk_vec_type(c.X.knots,     "c.X.knots",     "integer")
+  .chk_vec_type(c.Y.knots,     "c.Y.knots",     "integer")
+
+  # numeric vectors with bounds
+  .chk_range(N.rel.crit,     "N.rel.crit",     0, 1)
+  .chk_range(N.loc.rel.crit, "N.loc.rel.crit", 0, 1)
+  .chk_range(S.crit,         "S.crit",         0, 1)
+  .chk_range(S.rel.crit,     "S.rel.crit",     0, 1)
+
+  # numeric vectors (no bounds stated)
+  .chk_vec_type(Q.crit,        "Q.crit",        "numeric")
+  .chk_vec_type(c.Q.crit,      "c.Q.crit",      "numeric")
+  .chk_vec_type(c.crit.improv, "c.crit.improv", "numeric")
+
+  # c.corr.term bounds
+  .chk_range(c.corr.term, "c.corr.term", 0.01, 0.2)
+
+  # lists of functions
+  .chk_fun_list(generalize.f, "generalize.f")
+  .chk_fun_list(compare.f,    "compare.f")
+
+  # maximize: logical vector; if compare.f also provided, lengths must match
+  .chk_vec_type(maximize, "maximize", "logical")
+  if (!is.null(maximize) && !is.null(compare.f) && length(maximize) != length(compare.f)) {
+    .bad("maximize", paste0("must have the same length as 'compare.f' (", length(compare.f), ")."))
+  }
+
+  # method: character vector
+  .chk_vec_type(method, "method", "character")
+
+  invisible(TRUE)
 }
