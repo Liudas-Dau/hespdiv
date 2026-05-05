2c84a65544feb0a8ba8b428bb2578aaf4d1988f1
Liudas-Dau <liudasdau@gmail.comm>
2026-05-05 14:33:48 +0300

    fix decription & use of ... argument

--- R/plot_nullhespdiv.R
+++ R/plot_nullhespdiv.R
@@ -1,16 +1,42 @@
-#' Plot the results of nullhespdiv object
+#' Plot a nullhespdiv object
+#'
+#' @description Plot method for a \code{nullhespdiv} object.
 #'
 #' @method plot nullhespdiv
-#' @description  plot method for nullhespdiv class object
-#' @param x A nullhespdiv class object
-#' @param ... other arguments
-#' @return NULL
+#' @param x A \code{nullhespdiv} object.
+#' @param ... Additional arguments passed to \code{graphics::boxplot()}.
+#'
+#' @return No return value. Called for the side effect of plotting a
+#'   \code{nullhespdiv} object.
+#'
 #' @author Liudas Daumantas
 #' @family HespDiv visualization options
 #' @importFrom graphics boxplot points
+#' @examples
+#' test_results <- nulltest(example_hespdiv)
+#' plot(test_results)
 #' @export
-plot.nullhespdiv <- function(x, ...){
-  graphics::boxplot(x[[2]], ylim = range(c(x[[1]]$performance,
-                                    unlist(x[[2]]))), main = paste0("n = ",nrow(x[[2]])))
-  graphics::points(1:ncol(x[[2]]), x[[1]]$performance, col=2, pch= 19, cex = 1.5)
+plot.nullhespdiv <- function(x, ...) {
+  args <- list(...)
+
+  if (is.null(args$ylim)) {
+    args$ylim <- range(c(x[[1]]$performance, unlist(x[[2]])), na.rm = TRUE)
+  }
+
+  if (is.null(args$main)) {
+    args$main <- paste0("n = ", nrow(x[[2]]))
+  }
+
+  do.call(
+    graphics::boxplot,
+    c(list(x = x[[2]]), args)
+  )
+
+  graphics::points(
+    1:ncol(x[[2]]),
+    x[[1]]$performance,
+    col = 2,
+    pch = 19,
+    cex = 1.5
+  )
 }
